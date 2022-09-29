42c9f45b437f1b821f8c192bc284a04bf665ec77
Oleksandr Kosiuta <oleksandr.kosiuta@digicert.com>
2022-09-29 14:49:23 +0300

    init commit

---
+++ src/main/java/com/okosiuta/rabitmqpoc/RabitmqpocApplication.java
@@ -0,0 +1,13 @@
+package com.okosiuta.rabitmqpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitmqpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabitmqpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okosiuta/rabitmqpoc/RabitmqpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okosiuta.rabitmqpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitmqpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
